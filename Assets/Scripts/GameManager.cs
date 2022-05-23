98c3b9b086a79f853796c00b21d01224a190b0a3
Roy052 <32808714+Roy052@users.noreply.github.com>
2022-05-23 17:32:24 +0900

    Add Menu Scene, Update TMP
    What is TMP... I don't know.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score = 0;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void MenuToBattle()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
