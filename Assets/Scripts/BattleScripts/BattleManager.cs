dd6449899fa45d06fa73276dcce21e848c7b8fc3
crihit <rlagusgkr98@gmail.com>
2022-06-28 07:55:16 +0900

    battle 판정데이터 로드방식 변경

---
+++ Assets/Scripts/BattleScripts/BattleManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public float anglePf = 0.97f;
+    public float angleGr = 0.85f;
+
+    public int scPP = 10;
+    public int scPG = 7;
+    public int scPB = 1;
+    public int scGP = 5;
+    public int scGG = 3;
+    public int scGB = -1;
+    public int scB = -10;
+}
