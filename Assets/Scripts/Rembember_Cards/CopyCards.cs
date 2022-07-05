4c4c27a12879a3a59e71d9cc83e74f0b74f795e6
SUNGWHANYOON <ysh0214727@naver.com>
2022-07-05 14:35:59 +0900

    Card에 번호부여

--- Assets/Scripts/Rembember_Cards/CopyCards.cs
+++ Assets/Scripts/Rembember_Cards/CopyCards.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using System.Linq;
 public class CopyCards : MonoBehaviour
 {
     public GameObject Card_Front;
@@ -9,25 +10,39 @@ public class CopyCards : MonoBehaviour
     Vector3 MousePosition;
     public Camera Camera;
     int i, j, k;
-
+    int count = 0;
     List<GameObject> id_front = new List<GameObject>();
     List<GameObject> id_back = new List<GameObject>();
+
+    List<bool> flip = new List<bool>();
+    List<int> backNumber = new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18 };
+
+    List<int> mixed_backNumber = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
 
+        mixed_backNumber = backNumber.OrderBy(a => Guid.NewGuid()).ToList(); 
+
         for (i = -8; i <= 8; i = i + 2)
         {
-            for (j = -4; j <= 4; j = j + 2)
+            for (j = -3; j <= 4; j = j + 2)
             {
+                count += 1;
                 GameObject input_Front = Instantiate(Card_Front, new Vector3(i, j, -1), Quaternion.identity);
                 GameObject input_Back = Instantiate(Card_Back, new Vector3(i, j, 0), Quaternion.identity);
 
+                input_Front.name = count.ToString();
+                input_Back.name = count.ToString() + " Back";
+
                 id_front.Add(input_Front);
                 id_back.Add(input_Back);
+
+                flip.Add(false);
             }
         }
+
     }
 
     // Update is called once per frame
@@ -42,7 +57,12 @@ public class CopyCards : MonoBehaviour
 
             if (hit)
             {
-                Debug.Log(hit.transform.gameObject);
+                Debug.Log(hit.transform.gameObject.name);
+
+                int now_id = Convert.ToInt32(hit.transform.gameObject.name.ToString());
+                
+                Debug.Log(mixed_backNumber[now_id]);
+                flip[mixed_backNumber[now_id]] = true;
             }
         }
     }
